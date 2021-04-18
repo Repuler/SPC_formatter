524d1b2a56efaa2e023cfeec2d12acd85f65157f
TheChuckinator <S1483081@student.mcckc.edu>
2021-04-18 17:40:27 -0500

    added constructor to InstrumentMenu

--- src/components/instrument-menu.jsx
+++ src/components/instrument-menu.jsx
@@ -3,27 +3,23 @@ import React from 'react';
 import Checkbox from "./checkbox";
 
 class InstrumentMenu extends React.Component {
+    constructor(props) {
+        super(props)
 
-    getInstrumentList() {
-        const instruments = ['banjo', 'piano', 'guitar', 'violen :..('];
-        return instruments; // I imagine we want a function that can grab available instrument files.
+        this.state = { instruments: ['banjo', 'piano', 'guitar', 'violen :..('] };
     }
 
-    
-
-    createCheckbox() {
-        const instruments = this.getInstrumentList()
-        
-        return instruments.map(instrument => <Checkbox label={instrument} />)
+    createCheckboxes() {
+        return this.state.instruments.map(instrument => <Checkbox label={instrument} />)
     }
 
     render() {
         return (
             <div>
                 <h1>Test</h1>
-                <ol>
-                    {this.createCheckbox()}
-                </ol>
+                <form>
+                    {this.createCheckboxes()}
+                </form>
             </div>
         );
     }
