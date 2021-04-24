87b0698a6c1844cf1cd1569b8fd01865fbeb6502
nate13nate <nate13nate@gmail.com>
2021-04-24 15:11:31 -0500

    Of course I have to commit again now because Chuck has a pull request. Ridiculous.

--- src/api/note.js
+++ src/api/note.js
@@ -2,7 +2,7 @@ class Note {
     //For note to text conversion: Have a current octave variable and compare for proper placement of the < and > symbols.
     note = 'c'; //Valid values: c, d, e, f, g, a, b
     octave = 5; //Valid values: 1-6
-    location = 0; // measured in eighth notes (for you heathens (looking at you Charlie), that's the small column in the piano roll)
+    location = 1; // measured in eighth notes (for you heathens (looking at you Charlie), that's the small column in the piano roll)
         // basically, which column is it in on the piano roll
     duration = 4; // measured in eighth notes; how long the note is
     isSharp = false; // for me until nathan finishes what he's doing
--- src/components/TrackPianoRoll/PianoNote/index.jsx
+++ src/components/TrackPianoRoll/PianoNote/index.jsx
@@ -2,10 +2,6 @@ import React from 'react';
 import { PIANO_ROLL_HEIGHT } from '..';
 import { numToName } from '../PianoKey';
 
-function getKeyByValue(object, value) {
-  return Object.keys(object).find(key => object[key] === value);
-}
-
 const PianoNote = ({
   noteInfo,
   key,
@@ -15,9 +11,9 @@ const PianoNote = ({
       className="trackPianoNote"
       key={key}
       style={{
-        gridRow: `${noteInfo.location + 1} / span ${noteInfo.duration}`,
-        gridColumn: `${Math.abs(Object.entries(numToName).find(name => name[1] === `${noteInfo.note.toUpperCase()[0]}${noteInfo.isSharp ? '#' : ''}`)[0] + noteInfo.octave * 12 - PIANO_ROLL_HEIGHT)}
-          / span 1`, // the above calculation is incorrect
+        gridColumn: `${noteInfo.location + 1} / span ${noteInfo.duration}`,
+        gridRow: `${Math.abs(parseInt(Object.entries(numToName).find(name => name[1] === `${noteInfo.note.toUpperCase()[0]}${noteInfo.isSharp ? '#' : ''}`)[0]) + (noteInfo.octave - 1) * 12 - PIANO_ROLL_HEIGHT) + 2}
+          / span 1`,
       }}
     ></div>
   );
